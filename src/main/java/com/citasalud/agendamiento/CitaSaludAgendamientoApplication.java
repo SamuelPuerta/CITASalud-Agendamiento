efbf188fc63a062b36041b601c1f9ad9e0cab11b
SamuelPuerta <puertasamuel04@gmail.com>
2025-09-23 13:23:43 -0500

    Correct the project base - Agendamiento (Feature 1)

---
+++ src/main/java/com/citasalud/agendamiento/CitaSaludAgendamientoApplication.java
@@ -0,0 +1,13 @@
+package com.citasalud.agendamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitaSaludAgendamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitaSaludAgendamientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citasalud/agendamiento/CitaSaludAgendamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citasalud.agendamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitaSaludAgendamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
